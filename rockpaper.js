6f66d51413b7fdb49796ae3373f6b23c850e4191
Luis <barneydino55@gmail.com>
2025-03-04 12:38:14 -0800

    FIX: removed extra print no more misleading result

--- rockpaper.js
+++ rockpaper.js
@@ -7,15 +7,9 @@ function getHumanChoice() {
 }
 
 function getComputerChoice(min, max) {
-  // return rock paper scissors randomly using math function
-  // let rock = 1;
-  // let paper = 2;
-  // let scissor = 3;
   let computerChoice = Math.floor(Math.random() * (max - min) + min);
   return computerChoice;
 }
-
-console.log(getComputerChoice(1, 4));
 
 function convertHumanChoice(humanChoice) {
   if (humanChoice.toLowerCase() == "rock") {
@@ -28,64 +22,95 @@ function convertHumanChoice(humanChoice) {
   return 0;
 }
 
-function scenerio1(humanChoice, computerChoice) {
-  if (humanChoice == 1 && computerChoice == 2) {
-    console.log("You lose! Paper beats Rock"); // 1 is loss
-    return 1;
-  }
-  if (humanChoice == 2 && computerChoice == 1) {
-    console.log("You Win! Paper beats Rock"); // 2 is a win
-    return 2;
-  }
-}
+// function scenerio1(humanChoice, computerChoice) {
+//   if (humanChoice == 1 && computerChoice == 2) {
+//     console.log("You lose! Paper beats Rock"); // 1 is loss
+//     return 1;
+//   }
+//   if (humanChoice == 2 && computerChoice == 1) {
+//     console.log("You Win! Paper beats Rock"); // 2 is a win
+//     return 2;
+//   }
+// }
 
-function scenerio2(humanChoice, computerChoice) {
-  if (humanChoice == 1 && computerChoice == 3) {
-    console.log("You Win! Rock beats Scissors"); // 2 is win
-    return 2;
-  }
-  if (humanChoice == 3 && computerChoice == 1) {
-    console.log("You Lose! Rock beats Scissors"); // 1 is a loss
-    return 1;
-  }
-}
+// function scenerio2(humanChoice, computerChoice) {
+//   if (humanChoice == 1 && computerChoice == 3) {
+//     console.log("You Win! Rock beats Scissors"); // 2 is win
+//     return 2;
+//   }
+//   if (humanChoice == 3 && computerChoice == 1) {
+//     console.log("You Lose! Rock beats Scissors"); // 1 is a loss
+//     return 1;
+//   }
+// }
 
-function scenerio3(humanChoice, computerChoice) {
-  if (humanChoice == 2 && computerChoice == 3) {
-    console.log("You Lose! Scissors beats Paper"); // 1 is loss
-    return 1;
-  }
-  if (humanChoice == 3 && computerChoice == 2) {
-    console.log("You Win! Scissors beats Paper"); // 2 is a win
-    return 2;
-  }
-}
+// function scenerio3(humanChoice, computerChoice) {
+//   if (humanChoice == 2 && computerChoice == 3) {
+//     console.log("You Lose! Scissors beats Paper"); // 1 is loss
+//     return 1;
+//   }
+//   if (humanChoice == 3 && computerChoice == 2) {
+//     console.log("You Win! Scissors beats Paper"); // 2 is a win
+//     return 2;
+//   }
+// }
+
+// let rock = 1;
+// let paper = 2;
+// let scissor = 3;
 
 function playRound(humanChoice, computerChoice) {
   let convertedHC = convertHumanChoice(humanChoice);
-  let outcome;
+  // let outcome;
+
+  // First, check for a tie
   if (convertedHC === computerChoice) {
     console.log("Its a Tie!");
     return 0;
   }
-  if ((convertedHC == 1 && computerChoice == 2) || (convertedHC == 2 && computerChoice == 1)) {
-    // scenerio1(convertedHC, computerChoice);
-    outcome = scenerio1(convertedHC, computerChoice);
-    return outcome;
+
+  // Then check all winning scenarios
+  // Rock beats Scissors
+  if ((convertedHC === 1 && computerChoice === 3) ||
+    (convertedHC === 3 && computerChoice === 1)) {
+    if (convertedHC === 1) {
+      console.log("You Win! Rock beats Scissors");
+      return 2;
+    } else {
+      console.log("You Lose! Rock beats Scissors");
+      return 1;
+    }
   }
-  if ((convertedHC == 1 && computerChoice == 3) || (convertedHC == 3 && computerChoice == 1)) {
-    outcome = scenerio2(convertedHC, computerChoice);
-    return outcome;
+
+  // Paper beats Rock
+  if ((convertedHC === 1 && computerChoice === 2) ||
+    (convertedHC === 2 && computerChoice === 1)) {
+    if (convertedHC === 2) {
+      console.log("You Win! Paper beats Rock");
+      return 2;
+    } else {
+      console.log("You Lose! Paper beats Rock");
+      return 1;
+    }
   }
-  if ((convertedHC == 2 && computerChoice == 3) || (convertedHC == 3 && computerChoice == 2)) {
-    outcome = scenerio3(convertedHC, computerChoice);
-    return outcome;
+
+  // Scissors beats Paper
+  if ((convertedHC === 2 && computerChoice === 3) ||
+    (convertedHC === 3 && computerChoice === 2)) {
+    if (convertedHC === 3) {
+      console.log("You Win! Scissors beats Paper");
+      return 2;
+    } else {
+      console.log("You Lose! Scissors beats Paper");
+      return 1;
+    }
   }
 }
 
 const playerSelection = getHumanChoice();
 const computerSelection = getComputerChoice(1, 4);
 
-console.log(convertHumanChoice(playerSelection))
+console.log(`Player chose: ${playerSelection} (${convertHumanChoice(playerSelection)})`);
+console.log(`Computer chose: ${computerSelection}`);
 
 playRound(playerSelection, computerSelection);
